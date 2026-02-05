93dddef255a713b6c10c67edd5aa42a24038f782
pedrorvelloso <pedro_reis14@hotmail.com>
2026-02-05 20:33:01 -0300

    fix: check if its preview branch

--- lib/utils/get-base-url.ts
+++ lib/utils/get-base-url.ts
@@ -5,13 +5,14 @@
 export function getBaseUrl(): string {
   const env = process.env.VERCEL_ENV
   const isProductionUrl = env === 'production'
+  const isPreviewUrl = env === 'preview' && process.env.VERCEL_GIT_COMMIT_REF === 'preview'
 
   // Production: use the production URL
   if (isProductionUrl && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
-  if (env === 'preview') {
+  if (isPreviewUrl) {
     return `https://preview.${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
